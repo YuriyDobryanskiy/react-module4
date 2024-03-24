40d04cff6d85e7813f12fced3ff042583b5cf198
Yuriy Dobryanskiy <dobryansky@i.ua>
2024-03-24 22:18:26 +0200

    test1

--- src/components/ErrorMessage/ErrorMessage.jsx
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -1,11 +1,15 @@
-//import iziToast from 'izitoast';
+import iziToast from 'izitoast';
 //import 'izitoast/dist/css/iziToast.min.css';
 // import imgUrl from '../../img/octagon.svg';
 
 export function ErrorMessage(message) {
-  return (
-    <>
-      alert(message);
-    </>
-  );
+  iziToast.error({
+    theme: 'dark',
+    message: message,
+    timeout: 5000,
+    backgroundColor: '#EF4040',
+    // iconUrl: imgUrl,
+    messageColor: '#FAFAFB',
+    position: 'topRight',
+  });
 }

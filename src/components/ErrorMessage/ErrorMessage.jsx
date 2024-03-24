7ea1bf51427e87cc5c2243ca428bdc4cc6e55b17
Yuriy Dobryanskiy <dobryansky@i.ua>
2024-03-24 22:19:05 +0200

    test2

--- src/components/ErrorMessage/ErrorMessage.jsx
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -1,5 +1,5 @@
 import iziToast from 'izitoast';
-//import 'izitoast/dist/css/iziToast.min.css';
+import 'izitoast/dist/css/iziToast.min.css';
 // import imgUrl from '../../img/octagon.svg';
 
 export function ErrorMessage(message) {
